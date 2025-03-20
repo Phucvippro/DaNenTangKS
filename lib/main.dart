28f559b64644bcd25e9a1ce94f097a71c9030105
Phucvippro <nguyentronghanhphuc168213@gmail.com>
2025-03-20 07:34:33 +0700

    v6

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:app/features/authentication/presentation/bloc/auth_bloc.dart';
-import 'package:app/screens/welcome.dart';
+import 'package:app/features/authentication/presentation/pages/welcome.dart';
 import 'injection_container.dart' as di;
 
 void main() async {
